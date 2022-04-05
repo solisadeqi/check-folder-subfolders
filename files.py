5ed137559248649bbedcf262b59d750f467c072a
Sadeghi <ts-soleiman.sadeghi@rakuten.com>
2022-04-05 14:20:32 +0900

    filkes

--- files.py
+++ files.py
@@ -23,7 +23,7 @@ def files(day):
                     print(( str(index) + '-'+ '   '+time.ctime(os.path.getctime(root+'/'+ file)) +'     File Name :' +file +'\n'))
    
 
-days_input = input("Inser duration in day : ")
+days_input = input("Inser duration in day to be check : ") #it will check for last inserted day(s)
 files(days_input)
 
 
